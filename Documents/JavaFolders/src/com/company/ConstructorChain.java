d690bdecc4a3b5195ebd900a574c78cab5db9603
PrasadChalamalasetty <prasad.chalamalasetty10@gmail.com>
2021-01-10 16:12:34 +0530

    First Commit

---
+++ Documents/JavaFolders/src/com/company/ConstructorChain.java
@@ -0,0 +1,28 @@
+package com.company;
+
+
+import java.util.LinkedList;
+
+class ConstructorChain {
+
+    public static void main(String[] args) {
+LinkedList <Float> random = new LinkedList<Float>();
+         random.add(2.0f);
+         random.add(4.0f);
+         random.add(5.0f);
+         random.add(10.0f);
+         random.add(99.9f);
+
+
+        //Create an ArrayList using Generics here
+
+        printArray(random);
+    }
+
+    public static void printArray(LinkedList<Float> arr) {
+        for (Float s : arr) {
+            System.out.println(s);
+        }
+        //Complete the method declaration here
+    }
+}
